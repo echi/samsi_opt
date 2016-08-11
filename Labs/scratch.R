38443dcf23a8319f890c1cdc31b7bb360bb921c7
Eric Chi <eric.c.chi@gmail.com>
2016-08-11 08:03:00 -0400

    Adding supplemental files.

---
+++ Labs/scratch.R
@@ -0,0 +1,13 @@
+# 1. Clone myopt package from github
+# 2. Open package in RStudio (Upper left hand corner, Open Project, Select 'myopt')
+# 3. Type "> library(devtools)"
+# 4. Type "> document()"
+# 5. Upper menu bar (Build and Reload)
+# 6. Open myopt/R/lab1.R
+# 7. Fill in template functions
+
+# Notes
+# - You can use "=" or "<-"
+# - Type "?function_name" to get help on function_name
+
+library(myopt)
